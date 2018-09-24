af15823a6f865cf0933cc0d71d334315883dd63f
Felipe Guimaraes <guima.felipec@gmail.com>
2018-09-24 14:10:11 -0300

    added priori classifier

--- src/naivebayes.py
+++ src/naivebayes.py
@@ -44,13 +44,26 @@ class Naive_Bayes:
 	def query(self, genre, age, gender, ocupation):
 		probs = [1]*5
 		tot_rats = sum(self.n_stars)
+		tot_oc = sum(self.ocupation_freq[ocupation])
+		tot_gender = sum(self.gender_freq[gender])
+		tot_genre = sum(self.genre_freq[genre])
+		tot_age = sum(self.age_freq[age])
 		for ind, _ in enumerate(probs):
-			probs[ind] *= (self.ocupation_freq[ocupation][ind]/self.n_stars[ind])*(self.n_stars[ind]/tot_rats)/(sum(self.ocupation_freq[ocupation])/tot_rats)
-			probs[ind] *= (self.gender_freq[gender][ind]/self.n_stars[ind])*(self.n_stars[ind]/tot_rats)/(sum(self.gender_freq[gender])/tot_rats)
-			probs[ind] *= (self.age_freq[age][ind]/self.n_stars[ind])*(self.n_stars[ind]/tot_rats)/(sum(self.age_freq[age])/tot_rats)
-			probs[ind] *= (self.genre_freq[genre][ind]/self.n_stars[ind])*(self.n_stars[ind]/tot_rats)/(sum(self.genre_freq[genre])/tot_rats)
+			# P(R|X) = P(X|R)*P(R)/P(X)
+			probs[ind] *= (self.ocupation_freq[ocupation][ind]/self.n_stars[ind])*(self.n_stars[ind]/tot_rats)/(tot_oc/tot_rats)
+			probs[ind] *= (self.gender_freq[gender][ind]/self.n_stars[ind])*(self.n_stars[ind]/tot_rats)/(tot_gender/tot_rats)
+			probs[ind] *= (self.age_freq[age][ind]/self.n_stars[ind])*(self.n_stars[ind]/tot_rats)/(tot_age/tot_rats)
+			probs[ind] *= (self.genre_freq[genre][ind]/self.n_stars[ind])*(self.n_stars[ind]/tot_rats)/(tot_genre/tot_rats)
 		return probs
 
+	def priori_query(self, movie_id):
+		n = 0
+		tot = 0
+		for rate in self.ratings:
+			if rate.movie.id == movie_id:
+				n += 1
+				tot += rate.stars
+		return tot/n
 
 if __name__ == "__main__":
 	nb = Naive_Bayes()
